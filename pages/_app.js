82e988a06f2031370d615cb993c0c834071356df
Emil Nuutinen <emil@emilnuutinen.com>
2021-07-28 11:09:06 +0300

    Change font from JetBrains Mono to IBM Plex Mono

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import Layout from '../layouts/layout'
-import '@fontsource/jetbrains-mono'
+import '@ibm/plex'
 
 export default function MyApp ({ Component, pageProps }) {
   return (
